8def875fcdf16ea66849b51f30cf6a002bce2f53
Alexey Ustinov <TotKtoSmog513@gmail.com>
2024-09-07 15:36:12 +0300

    Изменение скрипта (движение по кругу)

--- GoDotSc.cs
+++ GoDotSc.cs
@@ -11,7 +11,13 @@ public partial class GoDotSc : Sprite2D
 	public override void _Process(double delta)
 	{
 		Rotation += _angularSpeed * (float)delta;
+
+		var velocity = Vector2.Up.Rotated(Rotation) * _speed;
+		Position += velocity * (float)delta;
+
+		GD.Print($"Position: {Position}");
 		GD.Print($"Rotation: {Rotation}");
+
 	}
 	public GoDotSc() => GD.Print("Hello, world!");
 }
